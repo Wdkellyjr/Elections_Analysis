e7acaebecb8e932efea64e68c86abb3f40fc4abc
Warren D Kelly Jr <wdkellyjr@warrens-mbp-2.myfiosgateway.com>
2021-09-15 00:56:57 -0400

    Addisng the election analysis.

--- PyPoll.py
+++ PyPoll.py
@@ -64,7 +64,7 @@ with open(file_to_save, "w") as txt_file:
             winning_count = votes
             winning_candidate = candidate_name
             winning_percentage = vote_percentage
-    # Print the winning candidates' results to the terminal
+    #Print the winning candidates' results to the terminal
     winning_candidate_summary = (
         f"-------------------------\n"
         f"Winner: {winning_candidate}\n"
